f661d80e21f5f097a08950ba979334fe542ac464
John <thehumantorch88@gmail.com>
2018-03-24 11:24:00 -0400

    axious stufffff

--- javascript/parkingAPI.js
+++ javascript/parkingAPI.js
@@ -15,6 +15,10 @@ $("#submit").on("click", function(event) {
 
     event.preventDefault();
 
+    //resets input box back to their default css if the form is submited.
+    $(".inputData").css({"background-color": "#ffffff", "color": "#000000"});
+
+
     var streetnumber = $("#streetnumber").val().trim();
     //console.log(streetnumber);
     var name = $("#nameofstreet").val().trim();
@@ -50,9 +54,6 @@ $("#submit").on("click", function(event) {
 
 // if all the fields are filled out then the initAPIs function will be called.
     if (runAPIs === true) {
-        //resets input box back to their default css if the form is submited.
-        $(".inputData").css({"background-color": "#ffffff", "color": "#000000"});
-        
         initAPIs();
     }
 
@@ -61,8 +62,6 @@ $("#submit").on("click", function(event) {
 // function that sends requests to our apis.
     function initAPIs() {
 
-    runcrimedata();
-
     var firstparturl = "https://maps.googleapis.com/maps/api/geocode/json?address="
     var apikey = "&key=AIzaSyBjGILdIXQ8XuCI_8-zYM1phGE-34kJP-k"
     var queryURL = firstparturl + streetnumber + "+" + name + "+," + city + ",+" + abbrevstate + apikey;
@@ -70,7 +69,15 @@ $("#submit").on("click", function(event) {
 
     // var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyBjGILdIXQ8XuCI_8-zYM1phGE-34kJP-k"
     
-    $.ajax({
+      axios.get(queryURL)
+  .then(function (response) {
+    console.log(response.data.results[0].geometry.location.lat)
+       console.log(response.data.results[0].geometry.location.lng);
+       latitude = response.data.results[0].geometry.location.lat;
+        longitude = response.data.results[0].geometry.location.lng;
+        runparkwhiz();
+  })
+    /*$.ajax({
       url: queryURL,
       method: 'GET'
     }).then(function(result) {
@@ -79,7 +86,7 @@ $("#submit").on("click", function(event) {
         latitude = result.results["0"].geometry.location.lat;
         longitude = result.results["0"].geometry.location.lng;
         runparkwhiz();
-        });
+        });*/
 
         function runparkwhiz () {
         var query2firstpart = "https://api.parkwhiz.com/v4/quotes/?q=coordinates:"
@@ -90,28 +97,9 @@ $("#submit").on("click", function(event) {
         // https://api.parkwhiz.com/v4/quotes/?q=coordinates:41.8857256,-87.6369590&start_time=2017-12-23T12:00&end_time=2017-12-23T20:00&api_key=62d882d8cfe5680004fa849286b6ce20
 
         console.log(queryURL2);
-        $.ajax({
-        url: queryURL2,
-        method: 'GET'
-        }).then(function(result) {
+        axios.get(queryURL2).then(function(result) {
         console.log(result);
         });
-        }
-
-        function runcrimedata () {
-        var queryFirstURL = "https://api.usa.gov/crime/fbi/ucr/estimates/states/"+ abbrevstate + "?page=1&per_page=50&output=json&api_key=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv";
-            $.ajax({
-            url: queryFirstURL,
-            method: "GET"
-        })
-            .then(function (response) {
-                var results = response.results;
-                for (var i = 0; i < results.length; i++) {
-                    if (results[i].year == 2016) {
-                    console.log("year:", results[i].year, "number of car thefts:", results[i].motor_vehicle_theft,);
-                    }
-                }
-            })
         }
 
     }
@@ -119,8 +107,3 @@ $("#submit").on("click", function(event) {
     $(".inputData").val("")
 
 });
-
-
-
-
-
